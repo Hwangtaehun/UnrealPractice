5b383525f86758fd2ab1f9621e424bdb98ba28e1
Hwangtaehun <r283@naver.com>
2023-01-08 21:22:04 +0900

    TPS게임 제작 준비

---
+++ TPSProject/Intermediate/Build/Win64/UE4Editor/Inc/TPSProject/TPSProjectGameModeBase.gen.cpp
@@ -0,0 +1,87 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "TPSProject/TPSProjectGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeTPSProjectGameModeBase() {}
+// Cross Module References
+	TPSPROJECT_API UClass* Z_Construct_UClass_ATPSProjectGameModeBase_NoRegister();
+	TPSPROJECT_API UClass* Z_Construct_UClass_ATPSProjectGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_TPSProject();
+// End Cross Module References
+	void ATPSProjectGameModeBase::StaticRegisterNativesATPSProjectGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_ATPSProjectGameModeBase_NoRegister()
+	{
+		return ATPSProjectGameModeBase::StaticClass();
+	}
+	struct Z_Construct_UClass_ATPSProjectGameModeBase_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_ATPSProjectGameModeBase_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_TPSProject,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ATPSProjectGameModeBase_Statics::Class_MetaDataParams[] = {
+		{ "Comment", "/**\n * \n */" },
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "TPSProjectGameModeBase.h" },
+		{ "ModuleRelativePath", "TPSProjectGameModeBase.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_ATPSProjectGameModeBase_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<ATPSProjectGameModeBase>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_ATPSProjectGameModeBase_Statics::ClassParams = {
+		&ATPSProjectGameModeBase::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009002ACu,
+		METADATA_PARAMS(Z_Construct_UClass_ATPSProjectGameModeBase_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_ATPSProjectGameModeBase_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_ATPSProjectGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_ATPSProjectGameModeBase_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ATPSProjectGameModeBase, 657161851);
+	template<> TPSPROJECT_API UClass* StaticClass<ATPSProjectGameModeBase>()
+	{
+		return ATPSProjectGameModeBase::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ATPSProjectGameModeBase(Z_Construct_UClass_ATPSProjectGameModeBase, &ATPSProjectGameModeBase::StaticClass, TEXT("/Script/TPSProject"), TEXT("ATPSProjectGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ATPSProjectGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ TPSProject/Intermediate/Build/Win64/UE4Editor/Inc/TPSProject/TPSProjectGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TPSPROJECT_TPSProjectGameModeBase_generated_h
+#error "TPSProjectGameModeBase.generated.h already included, missing '#pragma once' in TPSProjectGameModeBase.h"
+#endif
+#define TPSPROJECT_TPSProjectGameModeBase_generated_h
+
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_SPARSE_DATA
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_RPC_WRAPPERS
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATPSProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ATPSProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATPSProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TPSProject"), NO_API) \
+	DECLARE_SERIALIZER(ATPSProjectGameModeBase)
+
+
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATPSProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ATPSProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATPSProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TPSProject"), NO_API) \
+	DECLARE_SERIALIZER(ATPSProjectGameModeBase)
+
+
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATPSProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATPSProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATPSProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATPSProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATPSProjectGameModeBase(ATPSProjectGameModeBase&&); \
+	NO_API ATPSProjectGameModeBase(const ATPSProjectGameModeBase&); \
+public:
+
+
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATPSProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATPSProjectGameModeBase(ATPSProjectGameModeBase&&); \
+	NO_API ATPSProjectGameModeBase(const ATPSProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATPSProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATPSProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATPSProjectGameModeBase)
+
+
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_12_PROLOG
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_SPARSE_DATA \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_RPC_WRAPPERS \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_INCLASS \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_SPARSE_DATA \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	TPSProject_Source_TPSProject_TPSProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TPSPROJECT_API UClass* StaticClass<class ATPSProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TPSProject_Source_TPSProject_TPSProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
