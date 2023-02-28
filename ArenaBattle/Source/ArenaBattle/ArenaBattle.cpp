67f0210b3d5732cc2fb49c46c2a1d4adbb796f2e
Hwangtaehun <r283@naver.com>
2023-02-28 20:49:15 +0900

    챕터2완성 및 3부분 시작

--- ArenaBattle/Source/ArenaBattle/ArenaBattle.cpp
+++ ArenaBattle/Source/ArenaBattle/ArenaBattle.cpp
@@ -1,6 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ArenaBattle.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle, "ArenaBattle" );
+DEFINE_LOG_CATEGORY(ArenaBattle);
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ArenaBattle, "ArenaBattle");
